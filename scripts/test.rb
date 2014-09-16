74768a39e6194fdc8f1555e6091f0f4e4239dced
Jordan Curzon <curzonj@gmail.com>
2014-09-16 08:25:30 -0700

    script creates a job now

--- scripts/test.rb
+++ scripts/test.rb
@@ -7,6 +7,7 @@ FACTORY_UUID="d9c166f0-3c6d-11e4-801e-d5aa4697630f"
 
 spodb = Excon.new('http://localhost:5100', :persistent => true)
 inventory = Excon.new('http://localhost:5400', :persistent => true)
+build = Excon.new('http://localhost:5300', :persistent => true)
 
 begin
   obj = {
@@ -30,9 +31,35 @@ begin
     body: { type: METAL_UUID, quantity: 35 }.to_json,
     headers: { "Content-Type" => "application/json" },
     expects: [ 200 ]
-  ).body
+  )
 
   pp JSON.parse(inventory.get(path: '/inventory').body)
+
+  # TODO spodb should do this
+  build.post(
+    path: "/facilities",
+    body: {
+      uuid: uuid,
+      blueprint: FACTORY_UUID
+    }.to_json,
+    headers: { "Content-Type" => "application/json" },
+    expects: [ 201 ]
+  )
+
+  build.post(
+    path: "/jobs",
+    body: {
+      facility: uuid,
+      action: "manufacture",
+      quantity: 1,
+      target: 'ffb74468-7162-4bfb-8a0e-a8ae72ef2a8b',
+      inventory: 'default'
+    }.to_json,
+    headers: { "Content-Type" => "application/json" },
+    expects: [ 201 ]
+  )
+
+  pp JSON.parse(build.get(path: '/jobs').body)
 rescue Excon::Errors::HTTPStatusError
   puts $!.response.body
   raise
