79490d6757e920e81f4f878526945207eeddc84c
Jordan Curzon <curzonj@gmail.com>
2015-05-10 16:01:40 -0700

    fixes

--- scripts/truncate.sh
+++ scripts/truncate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo 'truncate space_objects; truncate solar_systems; truncate wormholes' | psql -e spacebox-spodb
+echo 'truncate space_objects, solar_systems, wormholes' | psql -e spacebox-spodb
 echo 'truncate solar_systems' | psql -e spacebox-map
 echo 'truncate inventories;' | psql -e spacebox-inventory
-echo 'truncate facilities; truncate jobs;' | psql -e spacebox-build
+echo 'truncate facilities, jobs;' | psql -e spacebox-build
 
 
 
