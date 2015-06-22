60341408d1294efe3e63e83afea708d18f8345a1
Jordan Curzon <curzonj@gmail.com>
2015-06-22 14:31:53 -0700

    fixed broken resetAccount

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,8 @@ set -x
 
 rm -f *.wtf-trace
 
+./api/node_modules/swagger-tools/bin/swagger-tools validate -v api/docs/swagger.json
+
 forego run ./scripts/truncate.sh
 forego run node api/bin/load_blueprints.js
 forego start
