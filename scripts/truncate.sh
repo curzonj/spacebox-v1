573d91a8f11c14f2a2f4151de4e03debec2c47f2
Jordan Curzon <curzonj@gmail.com>
2015-06-24 21:16:26 -0700

    fixed wormhole travel

--- scripts/truncate.sh
+++ scripts/truncate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 redis-cli -h $DOCKER_IP FLUSHDB
 echo 'truncate space_objects, solar_systems, wormholes, containers, facilities, jobs, items, blueprints, blueprint_perms' | psql -e spacebox
 
